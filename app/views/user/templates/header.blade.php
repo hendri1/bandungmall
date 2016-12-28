359a7679c9ce2e6937db66555cc04801ccad8dcf
Deta Aditya <muhammaddetaaditya@gmail.com>
2016-12-28 14:55:17 +0700

    First modification: layout and header

--- app/views/user/templates/header.blade.php
+++ app/views/user/templates/header.blade.php
@@ -1,241 +1,103 @@
-
-<!-- Fixed navbar start -->
-<!-- <meta name="viewport" content="user-scalable=0;"/> -->
-<div class="navbar navbar-tshop navbar-fixed-top megamenu" role="navigation">
-    <div class="navbar-top">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-sm-6 col-xs-6 col-md-6">
-                    <div class="pull-left ">
-                        <ul class="userMenu ">
-                            <li><a href="{{ URL::to('help') }}"> <span class="hidden-xs">Bantuan</span><i
-                                    class="glyphicon glyphicon-info-sign hide visible-xs "></i> </a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-sm-6 col-xs-6 col-md-6 no-margin no-padding">
-                    <div class="pull-right">
-                        <ul class="userMenu">
-                            <li class="hidden-xs"><a href="#" class="search-trigger" data-toggle="modal" data-target="#ModalSearch"><i class="fa fa-search"> </i></a></li>
-                            @if (!Auth::check())
-                                <li><a href="#" data-toggle="modal" data-target="#ModalLogin"> <span class="hidden-xs">Login</span>
-                                    <i class="glyphicon glyphicon-log-in hide visible-xs "></i> </a></li>
-                                <li class="hidden-xs"><a href="#" data-toggle="modal" data-target="#ModalSignup"> Register </a></li>
-                            @else
-                                <li><a href="{{ URL::to('user/myAccount') }}">{{ Auth::user()->first_name. ' ' . Auth::user()->last_name }}</a></li>
-                                <li><a href="{{ URL::to('user/doLogout') }}">Logout <i class="fa fa-sign-out"></i></a></li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
-            </div>
+<!-- Header -->
+  <header class="header-style-2"> 
+    <!-- Top Bar -->
+    <div class="top-bar">
+      <div class="container"> 
+        <!-- Language -->
+        <div class="language"> <a href="{{ URL::to('help') }}">BANTUAN</a></div>
+        <div class="top-links">
+          <ul>            
+            @if (!Auth::check())
+            <li><a href="#" data-toggle="modal" data-target="#ModalLogin"> <span class="hidden-xs">Login</span><i class="glyphicon glyphicon-log-in hide visible-xs "></i> </a></li>
+            <li class="hidden-xs"><a href="#" data-toggle="modal" data-target="#ModalSignup"> Register </a></li>
+            @else
+            <li><a href="{{ URL::to('user/myAccount') }}">{{ Auth::user()->first_name. ' ' . Auth::user()->last_name }}</a></li>
+            <li><a href="{{ URL::to('user/doLogout') }}">Logout <i class="fa fa-sign-out"></i></a></li>
+            @endif
+          </ul>
         </div>
+      </div>
     </div>
-    <!--/.navbar-top-->
-
-    <div class="container" id="accordion">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
-                    class="sr-only"> Toggle navigation </span> <span class="icon-bar"> </span> <span
-                    class="icon-bar"> </span> <span class="icon-bar"> </span></button>
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-cart"><i
-                    class="fa fa-shopping-cart colorWhite"> </i> <span
-                    class="cartRespons colorWhite"> Cart (
-                        @if(Session::has('cartQty'))
-                            {{Session::get('cartQty')}}
-                        @else
-                            0
-
-                        @endif
-                        ) </span></button>
-            <a class="navbar-brand " href="{{ URL::to('/') }}"> <img src="{{ asset('public/assets/common/images/logo-small.jpg') }}" alt="Bandungmall.co.id"> </a>
-
-            <!-- this part for mobile -->
-            <div class="search-box pull-right hidden-lg hidden-md hidden-sm">
-                <div class="input-group">
-                    <button class="btn btn-nobg getFullSearch" type="button" data-toggle="modal" data-target="#ModalSearch"><i class="fa fa-search"> </i></button>
-                </div>
-            </div>
-        </div>
-
-        <!-- this part is duplicate from cartMenu  keep it for mobile -->
-        <div class="navbar-cart  collapse">
-            <div class="cartMenu  col-lg-4 col-xs-12 col-md-4 ">
-                <!--/.miniCartTable-->
-
-                <div class="miniCartFooter  miniCartFooterInMobile text-right">
-                    <h3 class="text-right subtotal"> 
-                        @if(Session::has('cartQty'))
-                            Terdapat {{Session::get('cartQty')}} barang dalam Cart
-                        @else
-                            Cart masih kosong
-                        @endif
-                         </h3>
-                </div>
-                <!--/.miniCartFooter-->
-
-            </div>
-            <!--/.cartMenu-->
-        </div>
-        <!--/.navbar-cart-->
-
-        <style>
-
-        </style>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="dropdown "><a data-toggle="dropdown" class="dropdown-toggle" href="#"> Wanita
-                    <b class="caret"> </b> </a>
-                    <ul class="dropdown-menu">
-                        @foreach ($data_category_child as $child)
-                            @if ($child->parent === 1)
-                                <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
-                            @endif
-                        @endforeach
-                    </ul>
-                </li>
-                <li class="dropdown "><a data-toggle="dropdown" class="dropdown-toggle" href="#"> Pria
-                    <b class="caret"> </b> </a>
-                    <ul class="dropdown-menu">
-                        @foreach ($data_category_child as $child)
-                            @if ($child->parent === 2)
-                                <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
-                            @endif
-                        @endforeach
-                    </ul>
-                </li>
-                <!-- change width of megamenu = use class > megamenu-fullwidth, megamenu-60width, megamenu-40width -->
-                <li class="dropdown megamenu-80width "><a data-toggle="dropdown" class="dropdown-toggle" href="#"> All Categories
-                    <b class="caret"> </b> </a>
-                    <ul class="dropdown-menu">
-                        <li class="megamenu-content">
-
-                            <!-- megamenu-content -->
-                            @foreach ($data_category_parent as $key=>$parent)
-                            <ul class="col-lg-4  col-sm-4 col-md-4  unstyled noMarginLeft">
-                                <li>
-                                    <p><strong>{{ $parent->name }}</strong></p>
-                                </li>
-                                @foreach ($data_category_child as $child)
-                                    @if ($child->parent === $parent->id)
-                                        <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                            @endforeach
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-            <!--- this part will be hidden for mobile version -->
-            <div class="nav navbar-nav navbar-right hidden-xs">
-                <div class="dropdown  cartMenu "><a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i
-                        class="fa fa-shopping-cart"> </i> <span class="cartRespons"> Cart (
-                        @if(Session::has('cartQty'))
-                            {{Session::get('cartQty')}}
-                        @else
-                            0
-                        @endif
-                        ) </span> <b
-                        class="caret"> </b> </a>
-
-                    <div class="dropdown-menu col-lg-4 col-xs-12 col-md-4 ">
-
-                        <div class="miniCartFooter text-right">
-                            <h3 class="text-center subtotal"> <a href="{{ URL::to('/cart') }}" >
-                                @if(Session::has('cartQty'))
-                                    Terdapat {{Session::get('cartQty')}} barang dalam Cart<br>
-                                    
-                                @else
-                                    Cart masih kosong
-                                @endif
-                            </a> </h3>
-                        </div>
-                        <!--/.miniCartFooter-->
-
-                    </div>
-                    <!--/.dropdown-menu-->
-                </div>
-                <!--/.cartMenu-->
-<!-- 
-                <div class="search-box">
-                    <div class="input-group">
-                        <button class="btn btn-nobg getFullSearch" type="button"><i class="fa fa-search"> </i></button>
-                    </div>
-
-                </div> -->
-                <!--/.search-box -->
-            </div>
-            <!--/.navbar-nav hidden-xs-->
-        </div>
-        <!--/.nav-collapse -->
-        <!-- Wanita ID equals 1 -->
-        <div id="collapseWanita" class="col-md-12 uppercase xlarge categoryChoice collapse">
-            <table class="col-md-12" style="margin:0;">
-                <tr>
-                @foreach ($data_category_child as $child)
-                    @if ($child->parent === 1)
-                        <td bgcolor="#ebebeb" align="center"><a href="{{ URL::to('product?category_id=' . $child->id) }}"><b><font color="black">{{ $child->name }}</font></b></a></li>
-                    @endif
-                    
-                @endforeach
-                </tr>
-            </table>
-        </div>
-        <div id="collapsePria" class="col-md-12 uppercase xlarge categoryChoice collapse">
-            <table class="col-md-12" style="margin:0;">
-                <tr>
-                @foreach ($data_category_child as $child)
-                    @if ($child->parent === 2)
-                        <td bgcolor="#ebebeb" align="center"><a href="{{ URL::to('product?category_id=' . $child->id) }}"><b><font color="black">{{ $child->name }}</font></b></a></li>
-                    @endif
-                    
-                @endforeach
-                </tr>
-            </table>
-        </div>
-
-    </div>
-    <!--/.container -->
     
-</div>
-<!-- /.Fixed navbar  -->
-        
+    <!-- Logo -->
+    <div class="container">
+      <div class="logo"> <a href="{{ URL::to('/') }}"> <img src="{{ asset('public/assets/common/images/logo-large.png') }}" alt="Bandungmall.co.id"> </a> </div>
+      </div>
+      
+         <div class="sticky">
+      <div class="container">
 
-<script type="text/javascript">
-/*$(document).on('click', '.accordion-toggle', function(event) {
-        event.stopPropagation();
-        var $this = $(this);
+      <!-- Nav -->
+      <!-- Nav -->
+        <nav class="webimenu"> 
+          <!-- MENU BUTTON RESPONSIVE -->
+          <div class="menu-toggle"> <i class="fa fa-bars"> </i> </div>
+          <ul class="ownmenu">
 
-        var parent = $this.data('parent');
-        var actives = parent && $(parent).find('.collapse.in');
+          <li><a href="#.">WANITA <b class="caret"> </b></a>
+            <ul class="dropdown">
+              @foreach ($data_category_child as $child)
+                @if ($child->parent === 1)
+                  <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
+                @endif
+              @endforeach
+            </ul>
+          </li>
 
-        // From bootstrap itself
-        if (actives && actives.length) {
-            hasData = actives.data('collapse');
-            //if (hasData && hasData.transitioning) return;
-            actives.collapse('hide');
-        }
+          <li><a href="#.">PRIA <b class="caret"> </b></a>
+            <ul class="dropdown">
+              @foreach ($data_category_child as $child)
+                @if ($child->parent === 2)
+                  <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
+                @endif
+              @endforeach
+            </ul>
+          </li>
 
-        var target = $this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''); //strip for ie7
-
-        $(target).collapse('toggle');
-});
-$(document).ready(function(){
-    $('#accordion .collapse').on('show.bs.collapse', function (e) {            
-        var all = $('#accordion').find('.collapse');
-        var actives = $('#accordion').find('.in, .collapsing');
-        alert("Your book is overdue.");
-        all.each(function (index, element) {
-          $(element).collapse('hide');
-        });
-        actives.each(function (index, element) {                
-          $(element).collapse('show');                
-        });
-    });
-    $("#accordion").each(function() { 
-        alert("Your book is overdue"); 
-    });
-});*/   
-</script>
+          <li class="meganav"><a href="#.">ALL CATEGORIES <b class="caret"> </b></a> 
+              <!--======= MEGA MENU =========-->
+              <ul class="megamenu full-width">
+                <li class="row nav-post">
+                @foreach ($data_category_parent as $key=>$parent)
+                  <div class="col-sm-3">
+                    <h6>{{ $parent->name }}</h6>
+                    <ul>
+                    @foreach ($data_category_child as $child)
+                      @if ($child->parent === $parent->id)
+                          <li><a href="{{ URL::to('product?category_id=' . $child->id) }}">{{ $child->name }}</a></li>
+                      @endif
+                    @endforeach
+                    </ul>
+                  </div>
+                @endforeach
+                </li>
+              </ul>
+          </li>
+          
+          <!--======= Shopping Cart =========-->
+          <li class="shop-cart"><a href="#."><i class="fa fa-shopping-cart"></i></a> 
+            <span class="numb">
+              @if(Session::has('cartQty'))
+                {{Session::get('cartQty')}}
+              @else
+                0
+              @endif
+            </span>
+            <ul class="dropdown">
+              <li>
+                @if(Session::has('cartQty'))
+                    Terdapat {{Session::get('cartQty')}} barang dalam Cart<br>
+                @else
+                    Cart masih kosong
+                @endif
+              </li>
+            </ul>
+          </li>
+          <!--======= SEARCH ICON =========-->
+          <li class="search-nav"><a href="#" class="search-trigger" data-toggle="modal" data-target="#ModalSearch"><i class="fa fa-search"></i></a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+<!-- Header End -->
--- app/views/user/templates/layout.blade.php
+++ app/views/user/templates/layout.blade.php
@@ -1,109 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ asset('public/assets/common/images/icon.png') }}">
-    <title>Bandungmall - {{ $title or "" }}</title>
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('public/assets/user/bootstrap/css/bootstrap.css" rel="stylesheet') }}">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="{{ asset('public/assets/common/images/icon.png') }}">
+<title>Bandungmall - {{ $title or "" }}</title>
 
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('public/assets/user/css/style.css') }}" rel="stylesheet">
-    
-    @yield('stylesheet')
+<!-- FONTS ONLINE -->
+<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
-    <!-- Just for debugging purposes. -->
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+<!--MAIN STYLE-->
+<link href="{{ asset('public/assets/user/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/user/css/main.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/user/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/user/css/responsive.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/user/css/animate.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/user/css/font-awesome.min.css') }}" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="{{ asset('public/assets/user/css/custom.css') }}" rel="stylesheet">
+
+<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/user/rs-plugin/css/settings.css') }}" media="screen" />
+
+@yield('stylesheet')
+
+<!-- JavaScripts -->
+<script src="{{ asset('public/assets/user/js/modernizr.js') }}"></script>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- include pace script for automatic web page progress bar  -->
-
-    <script>
-        paceOptions = {
-            elements: true
-        };
-    </script>
-    <script src="{{ asset('public/assets/user/js/pace.min.js') }}"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body>
 
+@include('user.templates.loader')
+
 @include('user.templates.login_modal')
+
 @include('user.templates.signup_modal')
+
 @include('user.templates.search_modal')
+
+<!-- Page Wrap -->
+<div id="wrap"> 
 @include('user.templates.header')
 
-@yield('content')
+{{-- @yield('content') 
 
 <div class="white-bg gap"></div>
 @include('user.templates.footer')
+</div>
+
+--}}
 
 <!-- Le javascript
 ================================================== -->
 
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-<script src="{{ asset('public/assets/user/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/assets/user/js/bootstrap.min.js') }}"></script>
+<!-- Wrap End --> 
+<script src="{{ asset('public/assets/user/js/jquery-1.11.3.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/wow.min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/bootstrap.min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/own-menu.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/owl.carousel.min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/jquery.magnific-popup.min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/jquery.flexslider-min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/jquery.isotope.min.js') }}"></script> 
 
-<!-- jquery-migrate only for product details -->
-<script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
-
-<!-- include smoothproducts // product zoom plugin  -->
-<script type="text/javascript" src="{{ asset('public/assets/user/js/smoothproducts-thumb.js') }}"></script>
-
-<!-- include to give ratings -->
-<script src="{{ asset('public/assets/user/plugins/rating/bootstrap-rating.min.js') }}"></script>
-
-<!-- include jqueryCycle plugin -->
-<script src="{{ asset('public/assets/user/js/jquery.cycle2.min.js') }}"></script>
-
-<!-- include easing plugin -->
-<script src="{{ asset('public/assets/user/js/jquery.easing.1.3.js') }}"></script>
-
-<!-- include  parallax plugin -->
-<script type="text/javascript" src="{{ asset('public/assets/user/js/jquery.parallax-1.1.js') }}"></script>
-
-<!-- optionally include helper plugins -->
-<script type="text/javascript" src="{{ asset('public/assets/user/js/helper-plugins/jquery.mousewheel.min.js') }}"></script>
-
-<!-- include mCustomScrollbar plugin //Custom Scrollbar  -->
-<script type="text/javascript" src="{{ asset('public/assets/user/js/jquery.mCustomScrollbar.js') }}"></script>
-
-<!-- include icheck plugin // customized checkboxes and radio buttons   -->
-<script type="text/javascript" src="{{ asset('public/assets/user/plugins/icheck-1.x/icheck.min.js') }}"></script>
-
-<!-- include grid.js // for equal Div height  -->
-<script src="{{ asset('public/assets/user/js/grids.js') }}"></script>
-
-<!-- include carousel slider plugin  -->
-<script src="{{ asset('public/assets/user/js/owl.carousel.min.js') }}"></script>
-
-<!-- jQuery select2 // custom select   -->
-<script src="http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
-
-<!-- include touchspin.js // touch friendly input spinner component   -->
-<script src="{{ asset('public/assets/user/js/bootstrap.touchspin.js') }}"></script>
-
-<!-- include custom script for site  -->
-<script src="{{ asset('public/assets/user/js/script.js') }}"></script>
-
-@yield('javascript')
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5860a397ddb8373fd2b37fbc/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="{{ asset('public/assets/user/rs-plugin/js/jquery.themepunch.tools.min.js') }}"></script> 
+<script type="text/javascript" src="{{ asset('public/assets/user/rs-plugin/js/jquery.themepunch.revolution.min.js') }}"></script> 
+<script src="{{ asset('public/assets/user/js/main.js') }}"></script>
 </body>
 </html>
